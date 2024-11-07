2511feff2c6b5795c3e485909049182d71051ddb
Maax9o9 <maxrockogenial@gmail.com>
2024-11-07 01:11:47 -0600

    second commit

--- db.py
+++ db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Update the DATABASE_URL with the correct database name and credentials
 DATABASE_URL = "postgresql+psycopg2://postgress@localhost:5432/data4b"
 
 engine = create_engine(DATABASE_URL)
